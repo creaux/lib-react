85e07309774b786c5f4956097e15df8a4a1a37b8
Petr Juna <petr.juna@gmail.com>
2020-11-03 16:48:44 +0100

    fix(navbar): Buttons story

--- src/components/navigation-buttons.component.tsx
+++ src/components/navigation-buttons.component.tsx
@@ -8,7 +8,7 @@ export interface NavigationButtonsProps {
 export const NavigationButtons: FunctionComponent<NavigationButtonsProps> = ({
   buttons,
 }) => (
-  <form className="form-inline">
+  <form className="form-inline ml-auto mr-2">
     {buttons.map((button, i) => (
       <Button {...button} key={i} />
     ))}
